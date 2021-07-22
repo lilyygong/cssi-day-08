18b845f3a88917a00cff084f4a13f373362bc98d
Your Name <you@example.com>
2021-07-22 16:46:04 +0000

    read data from databasee

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,4 +1,8 @@
 const getMessages= () =>{
     const passcode = document.querySelector("#passcode")
-    console.log("button pressed", passcode.value)
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value', (snapshot) =>{
+        const data = snapshot.val()
+        console.log(data)
+    })
 }
