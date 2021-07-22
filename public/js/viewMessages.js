bbe868eada47d6fcde0f7ac618080bc3d6ebbb0a
Your Name <you@example.com>
2021-07-22 17:01:59 +0000

    found a match for the user's passcode

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -5,7 +5,9 @@ const getMessages= () =>{
         const data = snapshot.val()
         console.log(data)
         for (let key in data) {
-            console.log(key, data[key])
+            if (key === passcode.value){
+                console.log("match found")
+            }
         }
     })
 }
